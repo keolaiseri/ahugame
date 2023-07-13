30cd1a8bdc2944e4e9ed741a8e508922b02c20ec
keolaiseri <iserik@hawaii.edu>
2023-07-12 16:28:02 -1000

    working on movement

--- Ahu Game/Assets/Scripts/JoystickInput.cs
+++ Ahu Game/Assets/Scripts/JoystickInput.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class JoystickInput : MonoBehaviour
+public class JoystickInput : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-
-
     public RectTransform joystickBackground;
     public RectTransform joystickHandle;
 
@@ -14,12 +13,20 @@ public class JoystickInput : MonoBehaviour
     private Vector2 initialPosition;
     private Vector2 inputDirection;
 
+    private bool isDragging = false;
+
+    private float handleRadius;
+    private float maxDistance;
+
     private void Awake()
     {
         initialPosition = joystickBackground.position;
+
+        handleRadius = GetComponent<RectTransform>().rect.width * 0.5f;
+        maxDistance = (joystickBackground.rect.width - handleRadius) * 0.5f;
     }
 
-    public void OnpointerDown(PointerEventData eventData)
+    public void OnPointerDown(PointerEventData eventData)
     {
         OnDrag(eventData);
     }
@@ -31,8 +38,35 @@ public class JoystickInput : MonoBehaviour
 
         float bgRadius = joystickBackground.rect.width * 0.5f;
         float handleRadius = joystickHandle.rect.width * 0.5f;
-        fl
+        float maxDistance = bgRadius - handleRadius;
+
+        Vector2 handlePosition = inputDirection * maxDistance;
+        transform.localPosition = handlePosition;
+
+        playerMovement.SetMovementDirection(inputDirection);
+        isDragging = true;
     }    
+    
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+        if (!isDragging)
+        {
+            //Handle mouse click input without dragging
+            if(!isDragging){
+                Vector2 input = eventData.position - initialPosition;
+                inputDirection = input.normalized;
+
+                playerMovement.SetMovementDirection(inputDirection);
+            }
+        }
+
+        isDragging = false;
+        inputDirection = Vector2.zero;
+        joystickHandle.position = initialPosition;
+        playerMovement.SetMovementDirection(inputDirection);
+    }
 
 
     // Start is called before the first frame update
